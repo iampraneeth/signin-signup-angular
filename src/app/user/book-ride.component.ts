5469233ece4473c25380adc8628c3bbe892ac6a4
Praneeth <rupavataram-ram.praneeth@capgemini.com>
2019-05-10 12:24:20 +0530

    signin signup angular

---
+++ src/app/user/book-ride.component.ts
@@ -0,0 +1,20 @@
+
+import { Component }from '@angular/core';
+
+import { UserService } from './user.service';
+
+
+@Component({
+
+    selector: "book-Ride",
+
+    templateUrl: "./book-ride.component.html"
+
+})
+
+export class BookRideComponent {
+
+
+
+}
+
